d62a5d6b492fdf7004b41284567a57ef63149863
Youssef Josten Herrera Vilchez <117321672+JostinHv@users.noreply.github.com>
2025-06-11 10:15:20 -0500

    Update deportes.php

--- api/secciones/deportes.php
+++ api/secciones/deportes.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $deportes = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Selección nacional se prepara para el Mundial de Fútbol con nueva estrategia táctica",
+"autor" => "Luis Martínez - Corresponsal Deportivo",
+"resumen" => "El entrenador ha implementado un sistema de juego innovador que combina presión alta con transiciones rápidas. Los jugadores han mostrado una adaptación excepcional durante los entrenamientos de la semana pasada.",
 ];
 ?>
