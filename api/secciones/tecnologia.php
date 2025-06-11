2f3d78aafada5ff87f54e50d69c1d66223edd49d
Youssef Josten Herrera Vilchez <117321672+JostinHv@users.noreply.github.com>
2025-06-11 10:16:09 -0500

    Update tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Nueva generación de chips cuánticos promete revolucionar la computación",
+"autor" => "Ing. Sandra Torres - Especialista en Tecnología",
+"resumen" => "IBM y Google han presentado procesadores cuánticos con 1000 qubits que podrían resolver problemas complejos en segundos. Esta tecnología tendrá aplicaciones en medicina, finanzas y criptografía.",
 ];
 ?>
