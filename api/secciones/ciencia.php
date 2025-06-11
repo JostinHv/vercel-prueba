1e8deb610f88bc709b7ebecc3c133b818be6e5a0
Youssef Josten Herrera Vilchez <117321672+JostinHv@users.noreply.github.com>
2025-06-11 10:14:52 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Descubren nueva especie de dinosaurio con plumas iridiscentes en Argentina",
+"autor" => "Dr. María González - Paleontóloga",
+"resumen" => "Un equipo internacional de científicos ha identificado una nueva especie de dinosaurio terópodo que vivió hace 90 millones de años. Los fósiles muestran evidencia de plumas con estructuras que habrían producido colores iridiscentes similares a los colibríes actuales.",
 ];
 ?>
