9b7059a7f294ecec860d867dd5af8ded0f13fabd
Youssef Josten Herrera Vilchez <117321672+JostinHv@users.noreply.github.com>
2025-06-11 10:15:35 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Las criptomonedas experimentan volatilidad tras nuevas regulaciones europeas",
+"autor" => "Ana Fernández - Analista Financiera",
+"resumen" => "Bitcoin y Ethereum han mostrado fluctuaciones significativas después de que la Unión Europea anunciara nuevas medidas regulatorias. Los expertos predicen una estabilización gradual en los próximos meses.",
 ];
 ?>
