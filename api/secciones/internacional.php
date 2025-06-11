d22126203cef22f331b890f0446125efd128985a
Youssef Josten Herrera Vilchez <117321672+JostinHv@users.noreply.github.com>
2025-06-11 10:15:45 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Cumbre climática en Dubai alcanza acuerdos históricos sobre energías renovables",
+"autor" => "Roberto Silva - Corresponsal Internacional",
+"resumen" => "Más de 190 países han firmado un compromiso para triplicar la capacidad de energía renovable para 2030. El acuerdo incluye un fondo de 100 mil millones de dólares para países en desarrollo.",
 ];
 ?>
