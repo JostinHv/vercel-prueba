2dce5eadf085679dda99fb8667faa1bfe2e92f1b
Youssef Josten Herrera Vilchez <117321672+JostinHv@users.noreply.github.com>
2025-06-11 10:15:05 -0500

    Update cultura.php

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "El Museo del Prado inaugura exposición sobre arte digital y tradición clásica",
+"autor" => "Carmen Rodríguez - Crítica de Arte",
+"resumen" => "La muestra 'Píxeles y Pinceles' presenta por primera vez una fusión entre obras maestras clásicas y creaciones digitales contemporáneas. La exposición estará abierta hasta marzo de 2025 y incluye experiencias de realidad virtual.",
 ];
 ?>
