fb65779ba3c6e2ab301c0a0b48db5726bfc033d0
Youssef Josten Herrera Vilchez <117321672+JostinHv@users.noreply.github.com>
2025-06-11 10:15:56 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Gobierno anuncia inversión millonaria en infraestructura de transporte público",
+"autor" => "Patricia López - Redacción Nacional",
+"resumen" => "El plan incluye la construcción de tres nuevas líneas de metro y la modernización de 200 estaciones de autobús. Las obras comenzarán en el segundo trimestre del año y generarán más de 15,000 empleos.",
 ];
 ?>
